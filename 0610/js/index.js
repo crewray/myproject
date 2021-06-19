262b2c68a0806e0cde77ce921496778dc7053743
lei <lei@qq.com>
2021-06-19 08:57:11 +0800

    更新了动态首页

---
+++ 0610/js/index.js
@@ -0,0 +1,190 @@
+var list = [{
+        "category": "科幻",
+        "director": "郭帆",
+        "premiereAt": "1549324800",
+        "name": "流浪地球",
+        "poster": "https://pic.maizuo.com/usr/movie/bed8f7edd414e5b8afd788be30b92f9f.jpg",
+        "grade": "8",
+        "language": "汉语普通话   英语",
+        "filmId": "5c934f35a91c9300688f2fdd",
+        "runtime": "100",
+        "filmType": "2D",
+        "isPresale": "true",
+        "isSale": "true",
+        "synopsis": "太阳即将毁灭，人类在地球表面建造出巨大的推进器，寻找新家园。然而宇宙之路危机四伏，为了拯救地球，为了人类能在漫长的2500年后抵达新的家园，流浪地球时代的年轻人挺身而出，展开争分夺秒的生死之战。",
+        "nation": "中国大陆 ",
+        "actorStr": "郭帆,屈楚萧,李光洁,吴孟达 ,赵今麦"
+    },
+    {
+        "category": "喜剧|动作",
+        "director": "韩寒",
+        "premiereAt": "1549324800",
+        "name": "飞驰人生",
+        "poster": "https://pic.maizuo.com/usr/movie/9e17a42916fe1fb80becc74736ea32ff.jpg",
+        "grade": "7",
+        "language": "汉语普通话",
+        "filmId": "5c934f5a0237d7007375e5c6",
+        "runtime": "100",
+        "filmType": "2D",
+        "isPresale": "true",
+        "isSale": "true",
+        "synopsis": "曾经在赛车界叱咤风云、如今却只能经营炒饭大排档的赛车手张驰决定重返车坛，在职业生涯的尾声挑战年轻一代的天才，他想聚齐昔日的搭档们，召唤神龙，再现光辉岁月，不料却遭现实狠狠打脸，状况百出。在这场还得从考驾照开始的复出之路上，究竟会有多少意料之外的窘境等待着这位过气车神？",
+        "nation": "中国大陆",
+        "actorStr": "韩寒 ,沈腾,尹正 ,尹昉 ,田雨 ,黄景瑜"
+    },
+    {
+        "category": "动作|科幻|冒险",
+        "director": "罗伯特·罗德里格兹",
+        "premiereAt": "1550793600",
+        "name": "阿丽塔：战斗天使",
+        "poster": "https://pic.maizuo.com/usr/movie/e2faa90be5a512de432b30d23398f052.jpg",
+        "grade": "7.6",
+        "language": "英语   西班牙语",
+        "filmId": "5c934f5afe88c2006529ddbc",
+        "runtime": "100",
+        "filmType": "2D",
+        "isPresale": "true",
+        "isSale": "true",
+        "synopsis": "阿丽塔（罗莎·萨拉扎尔 饰）醒来发现既不记得自己是谁，也不认识这个未来世界。善良的医生依德（克里斯托夫·沃尔兹 饰）收留了阿丽塔，因为他意识到在这被遗弃的机器人外壳下，潜藏着一个有着非凡的心灵。当阿丽塔逐步适应她的新生活和钢铁城的街头险恶时，依德想试图让她远离她那神秘的过往，但她的街头朋友雨果（基恩·约翰逊 饰）则试图帮她唤起回忆。城市腐败统治者对阿丽塔展开了追捕，她却因此发现了有关过去的线索——她具有独特而强大的战斗能力。若她能够逃离他们的控制，她就能成为拯救她的朋友，家人和她所爱世界的关键。",
+        "nation": "美国   加拿大 阿根廷",
+        "actorStr": "罗伯特·罗德里格兹, 克里斯托弗·沃尔兹,罗莎·萨拉查,基恩·约翰逊 ,詹妮弗·康纳利,艾德·斯克林"
+    },
+    {
+        "category": "剧情|喜剧|传记",
+        "director": "彼得·法雷里",
+        "premiereAt": "1551398400",
+        "name": "绿皮书 ",
+        "poster": "https://pic.maizuo.com/usr/movie/5cb44c8aa86c4d42ac62e708c2833908.jpg",
+        "grade": "7.3",
+        "language": "英语  意大利语   ",
+        "filmId": "5c934f35ba39c80073aa3dfd",
+        "runtime": "130",
+        "filmType": "2D",
+        "isPresale": "true",
+        "isSale": "true",
+        "synopsis": "一名黑人钢琴家，为前往种族歧视严重的南方巡演，找了一个粗暴的白人混混做司机。在一路开车南下的过程里，截然不同的两人矛盾不断，引发了不少争吵和笑料。但又在彼此最需要的时候，一起共渡难关。行程临近结束，两人也慢慢放下了偏见......　　绿皮书，是一本专为黑人而设的旅行指南，标注了各城市中允许黑人进入的旅店、餐馆。电影由真实故事改编。",
+        "nation": "美国",
+        "actorStr": "彼得·法雷里 ,马赫沙拉·阿里, 塞巴斯蒂安·马尼斯科 ,维果·莫腾森 , 琳达·卡德里尼 "
+    },
+    {
+        "category": "剧情|/|动画|/|奇幻",
+        "director": "大森贵弘  伊藤秀樹",
+        "premiereAt": "1551916800",
+        "name": "夏目友人帐",
+        "poster": "https://pic.maizuo.com/usr/movie/87c202d4d2b7cbd9fb53171cb8424e66.jpg",
+        "grade": "8",
+        "language": "日语",
+        "filmId": "5c934f5aba39c80073aa415d",
+        "runtime": "104",
+        "filmType": "2D",
+        "isPresale": "true",
+        "isSale": "true",
+        "synopsis": "在人与妖怪之间过着忙碌日子的夏目，偶然与以前的同学结城重逢，由此回忆起了被妖怪缠身的苦涩记忆。此时，夏目认识了在归还名字的妖怪记忆中出现的女性·津村容莉枝。和玲子相识的她，现在和独子椋雄一同过着平稳的生活。夏目通过与他们的交流，心境也变得平和。但这对母子居住的城镇，却似乎潜伏着神秘的妖怪。在调查此事归来后，寄生于猫咪老师身体的“妖之种”，在藤原家的庭院中，一夜之间就长成树结出果实。而吃掉了与自己形状相似果实的猫咪老师，竟然分裂成了3个——！？",
+        "nation": "日本",
+        "actorStr": "大森贵弘 ,神谷浩史,井上和彦 "
+    },
+    {
+        "category": "剧情",
+        "director": "白雪",
+        "premiereAt": "1552003200",
+        "name": "过春天",
+        "poster": "https://pic.maizuo.com/usr/movie/3af83238b58f0f463e18452b3405355f.jpg",
+        "grade": "8",
+        "language": "汉语普通话   粤语",
+        "filmId": "5c934f5a8d6d81006f3f2999",
+        "runtime": "99",
+        "filmType": "2D",
+        "isPresale": "true",
+        "isSale": "true",
+        "synopsis": "16岁单非家庭女孩“佩佩”，她的城市既是香港、也是深圳，一边有身份，一边有生活。为了和闺蜜的约定，为了自己的存在感，为了懵懂的好感，她内心的冲动被点燃、“水客”成为了她的另一个身份，一段颇有“冒险”感的青春故事就此开始。",
+        "nation": "中国大陆",
+        "actorStr": "白雪,黄尧,汤加文,孙阳 ,倪虹洁"
+    },
+    {
+        "category": "悬疑|惊悚",
+        "director": "杨盼盼  曹尚水",
+        "premiereAt": "1552608000",
+        "name": "少女宿舍",
+        "poster": "https://pic.maizuo.com/usr/movie/ae4905ac12d327a21b54b9e9e91bb4b6.jpg",
+        "grade": "7.9",
+        "language": "汉语普通话",
+        "filmId": "5c934f5a8d6d81006f3f29a9",
+        "runtime": "88",
+        "filmType": "2D",
+        "isPresale": "true",
+        "isSale": "true",
+        "synopsis": "大一新生李珍欣因为想成为一名语言治疗师，来到大学后发现同学对她都很不友好，除了一名叫尤美的学生，两人很快成为了好朋友。珍欣在入住的宿舍楼不断听到奇怪的声音，还有袭击珍欣的黑衣人，幸好最后被尤美救出，与此同时尤美的身份也随之被解开…",
+        "nation": "中国大陆",
+        "actorStr": "田启文,陈淋 ,姜皓文"
+    },
+    {
+        "category": "剧情|音乐|传记",
+        "director": "布莱恩·辛格",
+        "premiereAt": "1553212800",
+        "name": "波西米亚狂想曲",
+        "poster": "https://pic.maizuo.com/usr/movie/13acbd020f39eeca87d03c620120954b.jpg",
+        "language": "英语",
+        "filmId": "5c934f35ba39c80073aa3dfc",
+        "runtime": "135",
+        "filmType": "2D",
+        "isPresale": "true",
+        "isSale": "true",
+        "synopsis": "本片是对皇后乐队、传奇主唱弗雷迪·莫库里以及他们音乐的致敬盛宴，这是一段充满爱、痛苦、接纳和音乐的旅程。　　弗雷迪·莫库里（拉米·马雷克 饰）曾是希思罗机场的一名普通搬运工，对音乐满腔热血的他，与布莱恩·梅（格威利姆·李 饰）、罗杰·泰勒（本·哈迪 饰）、约 翰·迪肯（约瑟夫·梅泽罗 饰）组成皇后乐队。这个殿堂级乐队的从无到有，从疏离到重聚，从低谷到巅峰，仿佛就是弗雷迪一生的缩影。在最后的日子里，弗雷迪做了生命中最重要的决定——在音乐史上最伟大的“拯救生命”大型摇滚乐演唱会上，将所有热血付诸于歌声。",
+        "nation": "英国  美国",
+        "actorStr": "布莱恩·辛格 ,拉米·马雷克,本·哈迪,约瑟夫·梅泽罗"
+    },
+    {
+        "category": "剧情",
+        "director": "张栾",
+        "premiereAt": "1553212800",
+        "name": "老师·好",
+        "poster": "https://pic.maizuo.com/usr/movie/85f4fc27c6b90d7e84295e115837728b.jpg",
+        "language": "汉语普通话",
+        "filmId": "5c934f358d6d81006f3f2693",
+        "runtime": "100",
+        "filmType": "2D",
+        "isPresale": "true",
+        "isSale": "true",
+        "synopsis": "1985年的南宿一中，苗宛秋老师推自行车昂首走在校园，接受着人们艳羡的目光和纷至沓来的恭维。桀傲不驯的洛小乙、温婉可人的安静、新潮前卫的关婷婷、大智若愚的脑袋、舞痴兄弟文明建设、八婆海燕、奸商耗子……三班是一个永远也不缺故事的集体。苗宛秋怎么也不会想到，他即将走进的这个三班将会成为他以及他身边这辆自行车的噩梦。三班的同学也没有想到，这位新来的老师改变了他们的一生。",
+        "nation": "中国大陆",
+        "actorStr": "于谦 ,汤梦佳,秦鸣悦 ,王广源 ,孙艺杨 "
+    },
+    {
+        "category": "喜剧|动画|冒险",
+        "director": "迈克·米切尔",
+        "premiereAt": "1553212800",
+        "name": "乐高大电影2 ",
+        "poster": "https://pic.maizuo.com/usr/movie/c0f95ec62f77844e767a46d94f892285.jpg",
+        "language": "英语",
+        "filmId": "5c934f35fe88c2006529d9a6",
+        "runtime": "106",
+        "filmType": "2D",
+        "isPresale": "true",
+        "isSale": "true",
+        "synopsis": "风靡全球的经典动画系列新作《乐高大电影2》爆笑来袭。乐高小人艾米特贱萌回归，抱得美人归的他和女友露西过着甜蜜的生活。但是看似平静的表面其实暗藏阴谋，神秘外星入侵者突然降临，他们的破坏速度远在乐高小人的建造速度之上，艾米特的世界再度危在旦夕。外星入侵者甚至能洞察艾米特等人的每一次行动，困难与挑战直线升级，身手敏捷的露西也难逃厄运，陷入危机。艾米特必须联合一众好友，再次拯救世界。这一次，他们将前往遥远未知的新奇世界，见识更多令人大开眼界的乐高世界，同时也将发现真正的自我。",
+        "nation": "美国  澳大利亚  挪威  丹麦",
+        "actorStr": "克里斯·帕拉特,伊丽莎白·班克斯,威尔·阿奈特"
+    }
+]
+
+str = ''
+
+for (var i in list) {
+    var item = list[i]
+    if(!item.grade){
+        item.grade='无'
+    }
+    str += `<a href="./detail.html" class="item pt-25 pb-10 flex jc-sb">
+    <img src=${item.poster} alt="" class="img ml-10">
+    <div class="t-box flex fg1 fdc jc-sa desc">
+        <p class="text  f18">${item.name}</p>
+        <p class="text f14 f999">观众评分 ${item.grade}</p>
+        <p class="text f14 f999 actors">主演：${item.actorStr}</p>
+        <p class="text f14 f999">${item.nation} | ${item.runtime}分钟</p>
+    </div>
+    <span class="buy flex jc-c aic mr-10">购票</span>
+
+    </a>`
+}
+
+document.getElementById('movies').innerHTML=str
