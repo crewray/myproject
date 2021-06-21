1dd656ef8fbc64f591fdf03fada06c5512730ffb
lei <lei@qq.com>
2021-06-21 20:03:54 +0800

    修复了一些小bug

--- 0621/js/index.js
+++ 0621/js/index.js
@@ -1,6 +1,6 @@
 str = ''
 
-function getValue() {
+function show() {
     var value = this.innerText
     // if(value==='÷'){
     //     value='/'
@@ -26,8 +26,8 @@ function del(){
 }
 
 function equal(){
-    str=str.replace('x','*')
-    str=str.replace('÷','/')
+    str=str.replaceAll('x','*')
+    str=str.replaceAll('÷','/')
     str=String(eval(str))
     input.value=str
 
@@ -38,17 +38,21 @@ var btns = document.getElementsByClassName('btn')
 for(var i of btns){
     var txt=i.innerText
     if(txt!=='C'&&txt!=='删除'&&txt!=='='){
-        i.addEventListener("click",getValue)
+        i.addEventListener("click",show)
+        continue
     }
     else if(txt==='C'){
         i.addEventListener("click",free)
+        continue
     }
     else if(txt==='删除'){
         i.addEventListener("click",del)
+        continue
     }
     else{
         i.addEventListener("click",equal)
     }
+    
 }
 
-// btns[0].addEventListener("click", getValue)
+// btns[0].addEventListener("click", show)
